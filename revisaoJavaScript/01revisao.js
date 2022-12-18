11bc0fb5718051f18af6978578317e5aee687336
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-12-18 14:51:13 -0300

    Add files via upload

---
+++ revisaoJavaScript/01revisao.js
@@ -0,0 +1,49 @@
+const meuNumero = 3
+
+const num1= 1;
+const num2= 2;
+
+const operacaoMatematica = num1  * num2
+
+console.log(operacaoMatematica)
+
+
+// pontos flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5
+
+const novaOperacao = num1 / numeroPontoFlutuante
+console.log(novaOperacao)
+
+//NaN -> not a number (não é um numero)
+
+const alura = "Alura";
+console.log(alura * num1);
+
+// string 
+
+const texto1 = 'Olá, Mundo';
+const texto2 = 'Ola, mundo!';
+const senha = 'senhaSuperSegura456!'
+const StringDeNumeros = "34567"
+
+// aspas simple e aspas duplas, podemos usar uma aspas dentro de outra aspas para fazer comentarios como no exemplo abaixo
+
+//const citacao = 'O Vagner disse "Oi!"';
+//console.log(citacao)
+
+//concatenaçao (+)
+const citacao = 'meu nome é '
+const MyName = "vagner"
+console.log(citacao + MyName);
+
+
+//template string OU template literal
+//pesquisa
+
+//atualizar no git
+
+
+
+const nome = 
